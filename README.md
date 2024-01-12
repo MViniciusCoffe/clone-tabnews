41dcd327127889f0552f5ec634e42f24cb8c39d2
Garoto Do Café <104383688+MViniciusCoffe@users.noreply.github.com>
2024-01-12 22:25:34 +0000

    criado scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 implementação do https://www.tabnews.com.br para o https://curso.dev.
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,20 @@
-function Home () {
-    return <b><h1>Meu primeiro site de Muitos! :D</h1></b>
+function Home() {
+  return (
+    <b>
+      <h1>Meu primeiro site de Muitos! :D</h1>
+    </b>
+  );
+}
+
+function Coisa() {
+  return (
+    <div>
+      <b>
+        {" "}
+        Eu te amo moorrr <p>sériooo</p>
+      </b>{" "}
+    </div>
+  );
 }
 
 export default Home;
