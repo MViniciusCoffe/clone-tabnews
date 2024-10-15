fab90a56a56462e353a2b0a66ab1fb5c246e2142
Garoto Do Café <104383688+MViniciusCoffe@users.noreply.github.com>
2024-10-15 11:00:12 +0000

    Troquei a página inicial pela ideia do projeto

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,18 @@
 function Home() {
   return (
-    <b>
-      <h1>Se você me ama, dá uma risadinha</h1>
-    </b>
+    <div>
+      <b>
+        <h1>Bem vindo a Primeira página do Projeto!</h1>
+      </b>
+      <h2>
+        Intúito do projeto:
+      </h2>
+      <p>
+        Estre projeto tem a ideia de ser um espaço para falarmos de jogos e 
+        tudo que envolve seu universo, desde o seu desenvolvimento até
+        reviews, dicas de gameplay entre outros.
+      </p>
+    </div>
   );
 }
 
