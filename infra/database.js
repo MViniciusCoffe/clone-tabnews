b7af5c64f18376e64b60c358f273309ba56c0a19
Marcus Vinícius <104383688+MViniciusCoffe@users.noreply.github.com>
2025-05-19 02:12:29 +0000

    add `databse.js` and `.env` files

---
+++ infra/database.js
@@ -0,0 +1,17 @@
+import { Client } from "pg";
+
+async function query(queryObject) {
+  const client = new Client({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+  });
+  await client.connect();
+  const result = await client.query(queryObject);
+  client.end()
+  return result;
+}
+
+export default { query: query, };
