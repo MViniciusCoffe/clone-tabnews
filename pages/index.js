5d70a5c7657b57b4c672de92e55f248dd51c98ba
Marcus Vinícius <104383688+MViniciusCoffe@users.noreply.github.com>
2025-05-30 14:28:34 +0000

    Linted code

--- pages/index.js
+++ pages/index.js
@@ -4,13 +4,11 @@ function Home() {
       <b>
         <h1>Bem vindo a Primeira página do Projeto!</h1>
       </b>
-      <h2>
-        Intúito do projeto:
-      </h2>
+      <h2>Intúito do projeto:</h2>
       <p>
-        Estre projeto tem a ideia de ser um espaço para falarmos de jogos e 
-        tudo que envolve seu universo, desde o seu desenvolvimento até
-        reviews, dicas de gameplay entre outros.
+        Estre projeto tem a ideia de ser um espaço para falarmos de jogos e tudo
+        que envolve seu universo, desde o seu desenvolvimento até reviews, dicas
+        de gameplay entre outros.
       </p>
     </div>
   );
