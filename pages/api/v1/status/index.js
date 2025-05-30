4db869f678737a55f8754988ab25eb1b19495eee
Marcus Vinícius <104383688+MViniciusCoffe@users.noreply.github.com>
2025-05-30 14:08:30 +0000

    Finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,32 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query('SELECT 1 + 1 as soma;');
-  console.log(result.rows)
-  response.status(200).json({ chave: "Hello World" });
+  const databaseVersion = await database.query("SHOW server_version;");
+  const databaseVersionRows = databaseVersion.rows[0].server_version;
+  const databaseMaxConnections = await database.query("SHOW max_connections;");
+  const databaseMaxConnectionsRows =
+    databaseMaxConnections.rows[0].max_connections;
+
+  const updatedAt = new Date().toISOString();
+
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseOpenedConnections = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+  const databaseOpenedConnectionsLength =
+    databaseOpenedConnections.rows[0].count;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersionRows,
+        max_connections: parseInt(databaseMaxConnectionsRows),
+        opened_connections: databaseOpenedConnectionsLength,
+      },
+    },
+  });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,27 @@
 test("GET to api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
-  expect(response.status).toBe(200)
-})
+  expect(response.status).toEqual(200);
+
+  const responseBody = await response.json();
+  expect(responseBody.updated_at).toBeDefined();
+
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(parsedUpdatedAt).toBe(responseBody.updated_at);
+
+  // Recuperar objeto database
+  expect(responseBody.dependencies.database).toBeDefined();
+
+  // Recuperar a versão da database
+  expect(responseBody.dependencies.database.version).toBeDefined();
+  expect(responseBody.dependencies.database.version).toBe("16.0");
+
+  // Recuperar as conexões máximas
+  expect(responseBody.dependencies.database.max_connections).toBeDefined();
+  expect(
+    responseBody.dependencies.database.max_connections,
+  ).toBeLessThanOrEqual(100);
+
+  // Recuperar as conexões abertas
+  expect(responseBody.dependencies.database.opened_connections).toBeDefined();
+  expect(responseBody.dependencies.database.opened_connections).toBe(1);
+});
