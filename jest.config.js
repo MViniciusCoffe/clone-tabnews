b09e3bc830b2bda8c35b2dadd5ad425e8ba35fad
Marcus Vinícius <104383688+MViniciusCoffe@users.noreply.github.com>
2025-10-02 15:14:35 +0000

    adds `/api/v1/migrations` endpoint

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+const nextJest = require("next/jest");
+const dotenv = require("dotenv");
+dotenv.config({
+  path: ".env.development",
+})
+
+
+const createJestConfig = nextJest({
+  dir: "./",
+});
+const jestConfig = createJestConfig({
+  moduleDirectories: ["node_modules", "<rootDir>"]
+});
+
+module.exports = jestConfig;
