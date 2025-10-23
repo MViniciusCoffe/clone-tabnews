be659805fe3baad7247c974235cbee0b8d13862f
Marcus Vinícius <104383688+MViniciusCoffe@users.noreply.github.com>
2025-10-23 20:05:44 +0000

    Makes `npm test` more robust with `async-retry` and `orchestrator.js`

--- infra/database.js
+++ infra/database.js
@@ -34,8 +34,8 @@ export default { query, getNewClient };
 function getSSLValues() {
   if (process.env.POSTGRES_CA) {
     return {
-      ca: process.env.POSTGRES_CA
-    }
+      ca: process.env.POSTGRES_CA,
+    };
   }
-  return process.env.NODE_ENV === 'production' ? true : false;
+  return process.env.NODE_ENV === "production" ? true : false;
 }
