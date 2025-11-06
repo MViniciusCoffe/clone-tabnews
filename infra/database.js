65c4311313fc9d7a30fab4d0f5be37db388ec6be
Marcus Vinícius <104383688+MViniciusCoffe@users.noreply.github.com>
2025-11-06 14:40:16 +0000

    fix `ESLint` linting

--- infra/database.js
+++ infra/database.js
@@ -29,7 +29,12 @@ async function getNewClient() {
   return client;
 }
 
-export default { query, getNewClient };
+const database = {
+  query,
+  getNewClient,
+};
+
+export default database;
 
 function getSSLValues() {
   if (process.env.POSTGRES_CA) {
