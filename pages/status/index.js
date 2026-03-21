706ca70b7e6f69ac514d2ec9d6c2b6009a6dac50
Marcus Vinícius <104383688+MViniciusCoffe@users.noreply.github.com>
2026-03-21 22:39:14 +0000

    feat: add initial version of `/status` page

---
+++ pages/status/index.js
@@ -0,0 +1,56 @@
+import useSWR from "swr";
+
+async function fetchApi(key) {
+  const response = await fetch(key);
+  const responseBody = await response.json();
+  return responseBody;
+}
+
+export default function StatusPage() {
+  return (
+    <>
+      <h1>Status</h1>
+      <UpdatedAt />
+      <DatabaseStatus />
+    </>
+  );
+}
+
+function UpdatedAt() {
+  const { isLoading, data } = useSWR("/api/v1/status", fetchApi, {
+    refreshInterval: 2000,
+  });
+
+  let updatedAtText = "Carregando...";
+
+  if (!isLoading && data) {
+    updatedAtText = new Date(data.updated_at).toLocaleString("pt-BR");
+  }
+
+  return <div>Última atualização: {updatedAtText}</div>;
+}
+
+function DatabaseStatus() {
+  const { isLoading, data } = useSWR("/api/v1/status", fetchApi, {
+    refreshInterval: 2000,
+  });
+
+  let databaseStatusInformation = "Carregando...";
+
+  if (!isLoading && data) {
+    databaseStatusInformation = (
+      <>
+        <p>Versão do banco de dados: {data.dependencies.database.version}</p>
+        <p>Conexões abertas: {data.dependencies.database.opened_connections}</p>
+        <p>Máximo de conexões: {data.dependencies.database.max_connections}</p>
+      </>
+    );
+  }
+
+  return (
+    <>
+      <h2>Database Status</h2>
+      <div>{databaseStatusInformation}</div>
+    </>
+  );
+}
